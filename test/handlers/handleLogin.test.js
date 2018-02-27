046a3cbc4ff18f697c8214b127cbafe64cbe1e03
Shubham Mathur <shubham.mathur@engineer.com>
2018-02-27 22:06:24 +0530

    Login Route: handleLogin: checkUserExists Tested for single entry in database.

--- test/handlers/handleLogin.test.js
+++ test/handlers/handleLogin.test.js
@@ -1,10 +1,16 @@
 const login = require('../../src/handlers/handleLogin');
+const Models = require('../../models');
 
+beforeEach(() => Models.users.create({ username: 'TestUser' }));
+afterEach(() => Models.users.destroy({ where: { username: 'TestUser' }, truncate: false, restartIdentity: true }));
 
 describe('function checkUserExists', () => {
   test('should return null as user doesnot exist in db', () =>
     login.checkUserExists('Shubham').then(data =>
       expect(data).toBeNull()));
+  test('should return username as it exist in db', () =>
+    login.checkUserExists('TestUser').then(data =>
+      expect(data.username).toBe('TestUser')));
 });
 
 describe('function handleLogin', () => {
