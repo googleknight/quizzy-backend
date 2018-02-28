ee190557148909cfe6ae0240cb1e694318ec504a
Shubham Mathur <shubham.mathur@engineer.com>
2018-02-28 09:01:27 +0530

    Calculate Route:handleCalculate test cases updated

--- test/handlers/handleCalculate.test.js
+++ test/handlers/handleCalculate.test.js
@@ -18,9 +18,19 @@ describe('function handleCalculate', () => {
 });
 
 describe('function getTotalCorrectResponses', () => {
-  test('should return total number of correct Responses from database',  =>
+  test('should return total number of correct Responses from database', done =>
     handleLogin('TestUser')
       .then(() => calculate.getTotalCorrectResponses('TestUser')
-        .then(total => 
-          expect(total).toBe(0))))
+        .then(total => expect(total).toBe(0))));
+});
+
+
+describe('function handleCalculate', () => {
+  test('should return total number of correct Responses and total questions from database', done =>
+    handleLogin('TestUser')
+      .then(() => calculate.handleCalculate('TestUser')
+        .then((total) => {
+          expect(total.answer).toBe(0);
+          done();
+        })));
 });
